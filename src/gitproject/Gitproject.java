8f1d413550369706cd7c4374b5ed5b5db5d42446
Alexey <Alexey@Notebook>
2015-12-23 22:43:06 +0400

    Добавлен вывод второго сообщения

--- src/gitproject/Gitproject.java
+++ src/gitproject/Gitproject.java
@@ -16,5 +16,6 @@ public class Gitproject {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Привет");
+        System.out.println("Еще одна строка");
     }
 }
